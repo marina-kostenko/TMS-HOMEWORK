18d124760f11b57872c728bb7b2ecd6b2d9ab19f
marina-kostenko <89marisha@gmail.com>
2024-03-21 13:22:54 +0300

    Square.java

--- src/main/homework5/DZ1/Square.java
+++ src/main/homework5/DZ1/Square.java
@@ -8,6 +8,20 @@ public class Square extends Rectangle {
     }
 
     @Override
+    public void setSide1(int side1)
+    {
+        super.setSide1(side1);
+        super.setSide2(side1);
+    }
+
+    @Override
+    public void setSide2(int side2)
+    {
+        super.setSide2(side2);
+        super.setSide2(side2);
+    }
+
+    @Override
     public double area()
     {
         return super.area();
